3e824978482b16d53afb241818dbd51d69f24174
megpara <meganparadowski@gmail.com>
2026-08-01 08:37:57 -0700

    Style mobile menu

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function MenuItem({ title, subtitleOne, subtitleTwo, link }) {
     return (
-        <Link className="flex gap-4 items-center justify-center md:justify-normal group" href={link}>
+        <Link className="flex gap-4 items-center justify-between md:justify-center md:justify-normal group" href={link}>
             <div className="flex gap-2 text-xl items-center text-[#bab5b1]">
-            <div className="opacity-100 md:group-hover:opacity-100 md:opacity-0 duration-1000">[</div>
-            <div className="italic lowercase text-center text-xs relative font-display">{subtitleOne}<br/>{subtitleTwo}</div>
-            <div className="opacity-100 md:group-hover:opacity-100 md:opacity-0 duration-1000">]</div>
+            <div className="hidden md:inline md:opacity-0 md:group-hover:opacity-100 duration-1000">[</div>
+            <div className="italic lowercase text-left md:text-center text-xs relative font-display">{subtitleOne}<br/>{subtitleTwo}</div>
+            <div className="opacity-0 md:group-hover:opacity-100 duration-1000">]</div>
             </div>
-            <div className="text-2xl md:text-4xl text-[#f7f5ee] menuItem">{title}</div>
+            <div className="text-xl md:text-3xl text-[#f7f5ee] menuItem">{title}</div>
         </Link>
     )
 }
