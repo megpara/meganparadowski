ffb443aa89e0c6c5bc7330b38d03c658b393c31a
megpara <meganparadowski@gmail.com>
2026-08-01 08:49:24 -0700

    Redo contact spacing

--- pages/contact.js
+++ pages/contact.js
@@ -13,7 +13,7 @@ export default function Ethos() {
                 />
             </Head>
             <div className="relative w-full h-full flex flex-col">
-                <div className="h-full flex flex-col pt-[20vh] md:pt-[24vh] px-4 md:px-8">
+                <div className="h-full flex flex-col py-[20vh] md:py-[24vh] px-4 md:px-8 my-24 md:my-0 justify-center items-center md:items-stretch text-center md:text-left">
                     <div className="flex flex-col md:flex-row gap-4">
                         <h1 className="basis-1/2">Contact</h1>
                         <div className="basis-1/2 italic text-[#bab5b1] text-sm">
